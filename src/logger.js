b94e37b5c51e7401bb79bf43595888b4767b489e
Florian Schnell <floschnell@gmail.com>
2017-04-23 12:13:44 +0200

    implemented own logger.

---
+++ src/logger.js
@@ -0,0 +1,60 @@
+export const LOG_LEVELS = {
+    DEBUG: 0,
+    INFO: 1,
+    WARN: 2,
+    ERROR: 3
+};
+
+let _verbosity = LOG_LEVELS.INFO;
+
+export const reduxLogger = verbosity => store => next => action => {
+    Logger.log(verbosity, 'executing action', action);
+    return next(action);
+};
+
+export default class Logger {
+
+    static setVerbosity(verbosity) {
+        _verbosity = verbosity;
+    }
+
+    static log(verbosity, ...args) {
+        switch(verbosity) {
+            case LOG_LEVELS.DEBUG:
+                Logger.debug.apply(null, args);
+                break;
+            case LOG_LEVELS.WARN:
+                Logger.warn.apply(null, args);
+                break;
+            case LOG_LEVELS.ERROR:
+                Logger.error.apply(null, args);
+                break;
+            default:
+                Logger.info.apply(null, args);
+        }
+    }
+
+    static debug(...args) {
+        if (_verbosity <= LOG_LEVELS.DEBUG) {
+            console.debug.apply(console, args);
+        }
+    }
+
+    static info(...args) {
+        if (_verbosity <= LOG_LEVELS.INFO) {
+            console.log.apply(console, args);
+        }
+    }
+
+    static warn(...args) {
+        if (_verbosity <= LOG_LEVELS.WARN) {
+            console.warn.apply(console, args);
+        }
+    }
+
+    static error(...args) {
+        if (_verbosity <= LOG_LEVELS.ERROR) {
+            console.error.apply(console, args);
+        }
+    }
+}
